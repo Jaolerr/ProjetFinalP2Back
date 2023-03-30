f7dc0ead8d1a5870d4675062ea50a4399a70b16b
=zwileo <=leo.zwilling@orange.fr>
2023-03-30 12:27:37 +0200

    création classes experiences et trajet

---
+++ src/main/java/com/inti/model/Experience.java
@@ -0,0 +1,43 @@
+package com.inti.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Experience {
+	
+	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_voyage;
+	private String destination;
+	private String description;
+	private String photo;
+	private double depense;
+	private double rating_moyen;
+	
+	
+	public Experience(String destination, String description, String photo, double depense, double rating_moyen) {
+		super();
+		this.destination = destination;
+		this.description = description;
+		this.photo = photo;
+		this.depense = depense;
+		this.rating_moyen = rating_moyen;
+	}
+	
+	
+	private double calcul_depense(double prix_r, double prix_t, double prix_l) {
+		
+		return prix_r + prix_t + prix_l;
+	}
+
+}
---
+++ src/main/java/com/inti/model/Trajet.java
@@ -0,0 +1,24 @@
+package com.inti.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Trajet {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_t;
+	private String ville_depart;
+	private String ville_arrivee;
+	private double prix_t;
+	
+}

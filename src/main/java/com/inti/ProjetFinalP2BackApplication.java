ac2248c7ca1868d88dc15aee1417ff7c6278455b
jeremy <jeremy.claude4@gmail.com>
2023-03-30 12:13:37 +0200

    test jenkins

--- src/main/java/com/inti/ProjetFinalP2BackApplication.java
+++ src/main/java/com/inti/ProjetFinalP2BackApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetFinalP2BackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetFinalP2BackApplication.class, args);
+		SpringApplication.run(ProjetFinalP2BackApplication.class, args); 
 	} 
 		  
 

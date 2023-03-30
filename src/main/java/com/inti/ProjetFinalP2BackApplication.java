74e8a0d0e3e1631445ba2375eadf6ffbf7e30fc8
jeremy <jeremy.claude4@gmail.com>
2023-03-30 11:22:11 +0200

    Premier commit

---
+++ src/main/java/com/inti/ProjetFinalP2BackApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalP2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFinalP2BackApplication.class, args);
+	}
+
+}

4f28e761d0abdc9622d45449ffcb8ecbc565d13f
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-30 16:15:44 +0200

    ville controller

--- src/main/java/com/inti/controller/VilleController.java
+++ src/main/java/com/inti/controller/VilleController.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.inti.model.Ville;
+
 import com.inti.repository.IVilleRepository;
 
 @RestController
@@ -23,6 +28,27 @@ public class VilleController {
 	public List<Ville> listeVille()
 	{
 		return ivr.findAll();
+	}
+	
+	
+	@PostMapping("saveVille")
+	public Ville saveVille(@RequestBody Ville v)
+	{
+	    Ville vSaved = ivr.save(v);
+		
+		return v;
+	}
+	
+	@DeleteMapping("deleteVille/{id}")
+	public void deleteArticle(@PathVariable("id_ville") int id_ville)
+	{
+		ivr.deleteById(id_ville);
+	}
+	
+	@GetMapping("villeId/{id}")
+	public Ville villeId(@PathVariable("id_ville") int id_ville)
+	{
+		return ivr.getReferenceById(id_ville);
 	}
 	
 	

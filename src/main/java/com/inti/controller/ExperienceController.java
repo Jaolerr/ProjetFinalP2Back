76e6c0a294e6307215d17322b1066001d8ea7aba
InfoJavaF <florian.belin@itech.fr>
2023-03-31 11:31:12 +0200

    Experience controller

--- src/main/java/com/inti/controller/ExperienceController.java
+++ src/main/java/com/inti/controller/ExperienceController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,33 @@ public class ExperienceController {
 	{
 		return ier.findAll();
 	}
+	
+	@PostMapping("saveExperience")
+	public Experience saveExperience(@RequestBody Experience e)
+	{
+		return ier.save(e);
+	}
+	
+	@DeleteMapping("deleteExperience/{id}")
+	public boolean deleteExperience (@PathVariable("id") int id)
+	{
+		boolean b = false;
+		ier.deleteById(id);
+		try {
+			ier.findById(id);
+		}
+		catch (Exception e) {
+			b=true;
+		}
+		
+		return b;
+	}
+	
+	@PutMapping("updateExperience")
+	public Experience updateExperience (@RequestBody Experience e)
+	{
+		return ier.save(e);
+	}
+//	@GetMapping("experienceIdVille/{idVille}")
 
 }

64d9ac15c2af80da7846969731375f21959ec8e4
InfoJavaF <florian.belin@itech.fr>
2023-03-31 09:30:41 +0200

    Lieux controller

--- src/main/java/com/inti/controller/GuideController.java
+++ src/main/java/com/inti/controller/GuideController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.model.Guide;
-import com.inti.model.Utilisateur;
 import com.inti.repository.IGuideRepository;
 
 
---
+++ src/main/java/com/inti/controller/LieuxController.java
@@ -0,0 +1,45 @@
+package com.inti.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.inti.model.Lieux;
+import com.inti.repository.ILieuxRepository;
+
+
+public class LieuxController {
+
+	@Autowired
+	ILieuxRepository ilr;
+	
+	@GetMapping("listeLieux")
+	public List<Lieux> listeLieux() {
+		return ilr.findAll();
+	}
+	@PostMapping("saveUtilisateur")
+	public Lieux saveLieux (@RequestBody Lieux l ) {
+		return ilr.save(l);
+	}
+	@DeleteMapping("deleteLieux/{id}")
+	public void deleteLieux(@PathVariable("id")int id)
+	{
+		ilr.deleteById(id);
+	}
+	@GetMapping("lieuxId/{id}")
+	public Lieux lieuxId(@PathVariable("id") int id)
+	{
+		return ilr.getReferenceById(id);
+	}
+	@PutMapping("updateLieux")
+	public Lieux updateLieux(@RequestBody Lieux l)
+	{
+		return ilr.save(l);
+	}
+}

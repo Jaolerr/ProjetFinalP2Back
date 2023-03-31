c798739e402b6873bc7dd8ebebee722a9ff48071
InfoJavaF <florian.belin@itech.fr>
2023-03-31 14:47:15 +0200

    Pays Controller

--- src/main/java/com/inti/controller/PaysController.java
+++ src/main/java/com/inti/controller/PaysController.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +19,7 @@ import com.inti.repository.IPaysRepository;
 
 @RestController
 @RequestMapping("pays")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PaysController {
 	
 	@Autowired
@@ -23,5 +30,30 @@ public class PaysController {
 		
 		return ipr.findAll();
 	}
+	@PostMapping("savePays")
+	public Pays savePays(@RequestBody Pays p)
+	{
+		return ipr.save(p);
+	}
+	@DeleteMapping("deletePays/{id}")
+	public boolean deletePays (@PathVariable("id")int id)
+	{
+		boolean b = false;
+		ipr.deleteById(id);
+		try {
+			ipr.findById(id);
+		}
+		catch (Exception e) {
+			b=true;
+		}
+		
+		return b;
+	}
+	@PutMapping("updatePays")
+	public Pays updatePays (@RequestBody Pays p)
+	{
+		return ipr.save(p);
+	}
+	
 
 }
--- src/main/java/com/inti/controller/VilleController.java
+++ src/main/java/com/inti/controller/VilleController.java
@@ -7,10 +7,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.inti.model.Lieux;
 import com.inti.model.Ville;
 import com.inti.repository.IVilleRepository;
 
@@ -31,12 +32,15 @@ public class VilleController {
 	public List<Ville> topVille(){
 		List<Ville> lv = ivr.findAll();
 		Collections.sort(lv,(o1,o2)->o1.getListGuide().size()-o2.getListGuide().size());
-		return lv;
-		
-			
+		return lv;	
 		}
-			
 	
+	@GetMapping("villeIdPays/{idPays}")
+	public List<Ville> getVparPays(@PathVariable int idPays){
+		
+		return ivr.getVilleByPays(idPays);
+			
+	}
 	}
 	
 	

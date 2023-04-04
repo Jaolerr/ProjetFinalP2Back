92f89c48f69e5c614b7be0feb1b546275d1e38a4
jeremy <jeremy.claude4@gmail.com>
2023-04-04 16:34:52 +0200

    Test associer exp à utilisateur

--- src/main/java/com/inti/repository/IUtilisateurRepository.java
+++ src/main/java/com/inti/repository/IUtilisateurRepository.java
@@ -1,11 +1,15 @@
 package com.inti.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.inti.model.Utilisateur;
 
 @Repository
 public interface IUtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
+	
+	@Query(value = "select * from a_users where a_users.login =?",nativeQuery = true )
+	Utilisateur getUserbyLogin(String login);
 
 }
